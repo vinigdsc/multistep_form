46bb900a6aa636437ddbb05d051f13ac33f91a41
Vinicius Caetano <viniciusgcae@gmail.com>
2023-02-22 15:01:06 -0300

    first commit

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+import "./ReviewForm.css";
+
+const ReviewForm = ({ data, updateFieldHandler }) => {
+  return (
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="unsatisfied"
+            name="review"
+            required
+            checked={data.review === "unsatisfied"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+          />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="neutral"
+            name="review"
+            required
+            checked={data.review === "neutral"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+          />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="satisfied"
+            name="review"
+            checked={data.review === "satisfied"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+            required
+          />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="very_satisfied"
+            name="review"
+            checked={data.review === "very_satisfied"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+            required
+          />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea
+          name="comment "
+          id="comment"
+          placeholder="Conte como foi sua experiencia com o produto"
+          value={data.comment || ""}
+          onChange={(e) => updateFieldHandler("comment", e.target.value)}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewForm;
---
+++ src/components/Thanks.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Thanks.css";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />,
+};
+
+const Thanks = ({ data }) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>Sua opinião é importante!</p>
+      <p>
+        Conclua sua avaliação para ganhar um cupom de 10% na sua proxima compra
+      </p>
+      <h3>Aqui está o resumo da sua avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+        {emojiData[data.review]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+        {data.comment}
+      </p>
+    </div>
+  );
+};
+
+export default Thanks;
